373630f00140e31597e324c7f64ce0ec9b805efe
krzysztof.kroczak <krzysztof.jan.kroczak@gmail.com>
2018-08-05 12:34:45 +0100

    alias for module is not needed

--- TetrisApp/Playground.fsx
+++ TetrisApp/Playground.fsx
@@ -1,22 +1,22 @@
 #load "XYArray.fs"
 open XYArray
 
-let initEl (x : int16<XYArray.x>) (y:int16<XYArray.y>) = 
+let initEl (x : int16<x>) (y:int16<y>) = 
     let x = int x
     let y = int y
     x*y
 
-let x = XYArray.init 3s<XYArray.x> 2s<XYArray.y> initEl
+let x = XYArray.init 3s<x> 2s<y> initEl
 
-XYArray.get 4s<XYArray.x> 1s<XYArray.y> x = None
-XYArray.get 2s<XYArray.x> 3s<XYArray.y> x = None
-XYArray.get 1s<XYArray.x> 1s<XYArray.y> x = Some 1
-XYArray.get 2s<XYArray.x> 1s<XYArray.y> x = Some 2
+XYArray.get 4s<x> 1s<y> x = None
+XYArray.get 2s<x> 3s<y> x = None
+XYArray.get 1s<x> 1s<y> x = Some 1
+XYArray.get 2s<x> 1s<y> x = Some 2
 
 
-let newXOutOfBound = XYArray.set 5s<XYArray.x> 1s<XYArray.y> 13 x 
+let newXOutOfBound = XYArray.set 5s<x> 1s<y> 13 x 
 newXOutOfBound = None
 
-let newX = XYArray.set 1s<XYArray.x> 1s<XYArray.y> 13 x 
-(XYArray.get 1s<XYArray.x> 1s<XYArray.y> (Option.get newX)) = Some 13
-(XYArray.get 2s<XYArray.x> 1s<XYArray.y> (Option.get newX)) = Some 2
+let newX = XYArray.set 1s<x> 1s<y> 13 x 
+(XYArray.get 1s<x> 1s<y> (Option.get newX)) = Some 13
+(XYArray.get 2s<x> 1s<y> (Option.get newX)) = Some 2
